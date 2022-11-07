4d587710c1b25dbaa85b90554930c57c0371ffd7
lalithcodelover <lalith.vlr@gmail.com>
2022-11-07 22:10:01 +0530

    practice

---
+++ src/components/Users/AddUser.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import './AddUser.css'
+import Errormsg from "./Errormsg";
+
+function AddUser(props) {
+const [enteredUsername,setUsername] = useState('')
+const [enteredAge,setAge] = useState('')
+const [error,setError] = useState(false)
+
+  const addUsername =(e) => {
+setUsername(e.target.value)
+  }
+
+  const addAge =(e) =>{
+setAge(e.target.value)
+  }
+
+  const submitUserHandler = (event)=>{
+    event.preventDefault();
+    if(enteredUsername.trim().length===0 || enteredAge.trim().length===0 || enteredAge < 1){
+      setError(true)
+      return;
+    }
+    props.onAddUser(enteredUsername,enteredAge)
+    setUsername('')
+    setAge('')
+  }
+  return (
+    <div>
+     {error && <Errormsg/> }
+    <form className="form" onSubmit={submitUserHandler}>
+      <div>
+        <label className="form-label" htmlFor="username">Username</label>
+        <input className="form-input" id="username" type="text" value={enteredUsername} onChange={addUsername}/>
+      </div>
+      <div>
+        <label className="form-label" htmlFor="age">Age</label>
+        <input className="form-input" id="age" type="number" value={enteredAge} onChange={addAge}/>
+      </div>
+      <button type="submit">Add User</button>
+    </form>
+    </div>
+  );
+}
+
+export default AddUser;
---
+++ src/components/Users/Errormsg.js
@@ -0,0 +1,19 @@
+
+function Errormsg() {
+
+    return (
+        <div>
+            <div>
+                <h2>Invalid Input</h2>
+            </div>
+            <div>
+                <p>Input values acnnot be empty</p>
+            </div>
+            <div>
+                <button>Ok</button>
+            </div>
+        </div>
+    )
+}
+
+export default Errormsg
